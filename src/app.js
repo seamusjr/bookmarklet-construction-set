5b7b8dbb01567a250a45d000e765bfe2b608b133
Seamus Roche <seamusjr@hotmail.com>
2011-04-28 14:18:23 -0500

    updated javascript

--- src/app.js
+++ src/app.js
@@ -5,4 +5,23 @@ var $ = jQuery.noConflict(true);
 $('head').append($('<style type="text/css"></style>').html(Styles));
 
 // Your application JS goes here
-$('body').append(Templates.example({ name: 'world' }));
+$('body').append(Templates.example({ name: 'Control panel' }));
+
+function showTimelineNav() {
+	var $tbtn = $('#nav-stories-toggle'),
+	    $scrim = $('.timeline_scrim');
+	$tbtn.addClass('open_');
+	$scrim.fadeIn(250).bind('click', function(ev) {
+		ev.preventDefault();
+		hideTimeline();
+	});
+	$('#stories-nav').show().find('.timeline').animate({ 'top': '0px' }, 250);
+};
+
+$('.advance-timeline').click(function(e) {
+	e.preventDefault();
+	//alert('hello');
+	$('.controls ol').css('position','absolute'); $('.controls ol').animate({'left': '-250px'}, 500);  
+	$('.viewer').animate({'left': '-11725px'}, 500);
+	showTimelineNav();
+});
